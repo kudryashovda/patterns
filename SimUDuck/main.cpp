d0b1881256fd1f034d356da15d833602a1175b26
Kudryashov Dmitrii <kudryashovda@simicon.com>
2021-10-31 17:16:42 +0300

    Game init. All is ok

---
+++ SimUDuck/main.cpp
@@ -0,0 +1,78 @@
+#include <iostream>
+#include <memory>
+#include <vector>
+
+using namespace std;
+
+class Duck {
+public:
+    Duck(string name)
+        : name_(move(name)) {
+    }
+
+    void quack() {
+        cout << name_ << ": QwaQwa\n";
+    };
+
+    void swim() {
+        cout << name_ << ": WouWou\n";
+    };
+    virtual void display() = 0;
+
+private:
+    string name_;
+};
+
+class RedheadDuck : public Duck {
+public:
+    RedheadDuck(string name)
+        : Duck(move(name)) {
+    }
+
+    void display() override {
+        cout << "RedheadDuck\n";
+    }
+};
+
+class MallardDuck : public Duck {
+public:
+    MallardDuck(string name)
+        : Duck(move(name)) {
+    }
+
+    void display() override {
+        cout << "MallardDuck\n";
+    }
+};
+
+class RubberDuck : public Duck {
+public:
+    RubberDuck(string name)
+        : Duck(move(name)) {
+    }
+
+    void display() override {
+        cout << "RubberDuck\n";
+    }
+};
+
+int main() {
+    vector<unique_ptr<Duck>> ducks;
+    ducks.emplace_back(new RedheadDuck("Bob"s));
+    ducks.emplace_back(new RedheadDuck("Justin"s));
+
+    ducks.emplace_back(new MallardDuck("Tom"s));
+    ducks.emplace_back(new MallardDuck("Anton"s));
+
+    ducks.emplace_back(new RubberDuck("Mark"s));
+    ducks.emplace_back(new RubberDuck("Jason"s));
+
+    for (const auto& duck : ducks) {
+        duck->display();
+        duck->swim();
+        duck->quack();
+        cout << '\n';
+    }
+
+    return 0;
+}
