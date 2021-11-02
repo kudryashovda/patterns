ed013caa6e5714e94a1cb234d003ca79fb58c146
Kudryashov Dmitrii <kudryashovda@simicon.com>
2021-11-02 22:06:37 +0300

    use pattern 'Observer'

--- WeatherData/weatherdata.cpp
+++ WeatherData/weatherdata.cpp
@@ -1,70 +1,141 @@
 #include <iostream>
+#include <set>
 
 using std::cout;
+using std::set;
 
-class CurrentConditionsDisplay {
+class Observer {
 public:
-    void update(float temp, float humidity, float pressure) {
-        cout << "Current conditions" << '\n';
-        cout << "Temperature: " << temp << '\n';
-        cout << "Humidity: " << humidity << '\n';
-        cout << "Pressure: " << pressure << '\n';
-        cout << '\n';
-    }
+    virtual void update(float temp, float humidity, float pressure) = 0;
 };
 
-class StatisticsDisplay {
+class Subject {
 public:
-    void update(float temp, float humidity, float pressure) {
-        cout << "Statistics" << '\n';
-        cout << temp << '\n';
-        cout << humidity << '\n';
-        cout << pressure << '\n';
-        cout << '\n';
-    }
+    virtual void registerObserver(Observer* observer) = 0;
+    virtual void removeObserver(Observer* observer) = 0;
+    virtual void notifyObservers() = 0;
 };
 
-class ForecastDisplay {
+class DisplayElement {
 public:
-    void update(float temp, float humidity, float pressure) {
-        cout << "Forecast" << '\n';
-        cout << "Tomorrow will be:" << '\n';
-        cout << "Temperature: " << temp << '\n';
-        cout << "Humidity: " << humidity << '\n';
-        cout << "Pressure: " << pressure << '\n';
-        cout << '\n';
-    }
+    virtual void display() = 0;
 };
 
-class WeatherData {
+class CurrentConditionsDisplay : public Observer, DisplayElement {
 public:
-    float getTemperature() {
-        return 25.0;
+    CurrentConditionsDisplay(Subject* weatherData) {
+        weatherData_ = weatherData;
+        weatherData->registerObserver(this);
     }
-    float getHumidity() {
-        return 80.0;
+
+    void display() override {
+        cout << "Current conditions: " << temperature_ << " C degrees and " << humidity_ << "% humidity" << '\n';
     }
-    float getPressure() {
-        return 770.0;
+
+    void update(float temp, float humidity, float pressure) override {
+        temperature_ = temp;
+        humidity_ = humidity;
+        (void)pressure;
+        display();
+    }
+
+private:
+    Subject* weatherData_;
+
+    float temperature_ = 0.0;
+    float humidity_ = 0.0;
+};
+
+class StatisticsDisplay : public Observer, DisplayElement {
+public:
+    StatisticsDisplay(Subject* weatherData) {
+        weatherData_ = weatherData;
+        weatherData->registerObserver(this);
+    }
+
+    void display() override {
+        cout << "Statistics: " << temperature_ << " " << humidity_ << " " << pressure_ << '\n';
+    }
+
+    void update(float temp, float humidity, float pressure) override {
+        temperature_ = temp;
+        humidity_ = humidity;
+        pressure_ = pressure;
+        display();
+    }
+
+private:
+    Subject* weatherData_;
+
+    float temperature_ = 0.0;
+    float humidity_ = 0.0;
+    float pressure_ = 0.0;
+};
+
+class ForecastDisplay : public Observer, DisplayElement {
+public:
+    ForecastDisplay(Subject* weatherData) {
+        weatherData_ = weatherData;
+        weatherData->registerObserver(this);
+    }
+
+    void display() override {
+        cout << "Tomorrow will be a good day (50%)" << '\n';
+    }
+
+    void update(float temp, float humidity, float pressure) override {
+        (void)temp;
+        (void)humidity;
+        pressure_ = pressure;
+        display();
+    }
+
+private:
+    Subject* weatherData_;
+
+    float pressure_ = 0.0;
+};
+
+class WeatherData : public Subject {
+public:
+    void registerObserver(Observer* observer) override {
+        observers_.insert(observer);
+    };
+    void removeObserver(Observer* observer) override {
+        observers_.erase(observer);
+    }
+    void notifyObservers() override {
+        for (auto* observer : observers_) {
+            observer->update(temperature_, humidity_, pressure_);
+        }
     }
 
     void measurementsChanged() {
-        CurrentConditionsDisplay currentConditionsDisplay;
-        StatisticsDisplay statisticsDisplay;
-        ForecastDisplay forecastDisplay;
-
-        float temp = getTemperature();
-        float humidity = getHumidity();
-        float pressure = getPressure();
-
-        currentConditionsDisplay.update(temp, humidity, pressure);
-        statisticsDisplay.update(temp, humidity, pressure);
-        forecastDisplay.update(temp, humidity, pressure);
+        notifyObservers();
     }
+
+    void setMeasurements(float temp, float humidity, float pressure) {
+        temperature_ = temp;
+        humidity_ = humidity;
+        pressure_ = pressure;
+
+        measurementsChanged();
+    }
+
+private:
+    set<Observer*> observers_;
+    float temperature_ = 0.0;
+    float humidity_ = 0.0;
+    float pressure_ = 0.0;
 };
 int main() {
     WeatherData weatherData;
-    weatherData.measurementsChanged();
+
+    CurrentConditionsDisplay currentConditionsDisplay(&weatherData);
+    StatisticsDisplay statisticsDisplay(&weatherData);
+    ForecastDisplay forecastDisplay(&weatherData);
+
+    weatherData.setMeasurements(80, 65, 30.4);
 
     return 0;
 }
