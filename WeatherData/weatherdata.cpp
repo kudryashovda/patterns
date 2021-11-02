4968e0d3d2160a92ff1786769dfc94877d906a3c
Kudryashov Dmitrii <kudryashovda@simicon.com>
2021-11-02 18:48:58 +0300

    first wrong realization

--- WeatherData/weatherdata.cpp
+++ WeatherData/weatherdata.cpp
@@ -1,26 +1,70 @@
 #include <iostream>
 
+using std::cout;
+
+class CurrentConditionsDisplay {
+public:
+    void update(float temp, float humidity, float pressure) {
+        cout << "Current conditions" << '\n';
+        cout << "Temperature: " << temp << '\n';
+        cout << "Humidity: " << humidity << '\n';
+        cout << "Pressure: " << pressure << '\n';
+        cout << '\n';
+    }
+};
+
+class StatisticsDisplay {
+public:
+    void update(float temp, float humidity, float pressure) {
+        cout << "Statistics" << '\n';
+        cout << temp << '\n';
+        cout << humidity << '\n';
+        cout << pressure << '\n';
+        cout << '\n';
+    }
+};
+
+class ForecastDisplay {
+public:
+    void update(float temp, float humidity, float pressure) {
+        cout << "Forecast" << '\n';
+        cout << "Tomorrow will be:" << '\n';
+        cout << "Temperature: " << temp << '\n';
+        cout << "Humidity: " << humidity << '\n';
+        cout << "Pressure: " << pressure << '\n';
+        cout << '\n';
+    }
+};
+
 class WeatherData {
 public:
     float getTemperature() {
-        measurementsChanged();
         return 25.0;
     }
     float getHumidity() {
-        measurementsChanged();
         return 80.0;
     }
     float getPressure() {
-        measurementsChanged();
         return 770.0;
     }
 
     void measurementsChanged() {
-        // here will be our code
+        CurrentConditionsDisplay currentConditionsDisplay;
+        StatisticsDisplay statisticsDisplay;
+        ForecastDisplay forecastDisplay;
+
+        float temp = getTemperature();
+        float humidity = getHumidity();
+        float pressure = getPressure();
+
+        currentConditionsDisplay.update(temp, humidity, pressure);
+        statisticsDisplay.update(temp, humidity, pressure);
+        forecastDisplay.update(temp, humidity, pressure);
     }
 };
 int main() {
     WeatherData weatherData;
+    weatherData.measurementsChanged();
 
     return 0;
 }
