a039c19d756b7db148e411eebe36ec6e09bba52c
Kudryashov Dmitrii <kudryashovda@simicon.com>
2021-11-04 18:48:27 +0300

    button and callback. use functional object

---
+++ Button/button.cpp
@@ -0,0 +1,45 @@
+#include <functional>
+#include <iostream>
+
+using std::cout;
+using std::function;
+
+class Button {
+public:
+    void click() {
+        if (func_) {
+            func_();
+        }
+    }
+    void bind(function<void()> func) {
+        func_ = func;
+    }
+
+private:
+    function<void()> func_ = nullptr;
+};
+
+void onBtn1Click() {
+    cout << "btn1 was clicked\n";
+}
+
+void onBtn2Click() {
+    cout << "btn2 was clicked\n";
+}
+
+int main() {
+
+    Button btn1;
+    Button btn2;
+    Button btn3;
+
+    btn1.bind(onBtn1Click);
+    btn2.bind(onBtn2Click);
+    btn3.bind([]() { cout << "btn3 was clicked\n"; });
+
+    btn1.click();
+    btn2.click();
+    btn3.click();
+
+    return 0;
+}
