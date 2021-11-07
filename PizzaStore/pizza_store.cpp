30fb7db596e237023e6c5b9f0053e95de885bc0c
Kudryashov Dmitrii <kudryashovda@simicon.com>
2021-11-07 22:08:07 +0300

    Simple fabric idioma. Beginning: show a problem

---
+++ PizzaStore/pizza_store.cpp
@@ -0,0 +1,64 @@
+#include <iostream>
+#include <memory>
+
+using std::make_unique;
+using std::unique_ptr;
+
+enum class PizzaType {
+    cheese,
+    greek,
+    pepperoni
+};
+
+class Pizza {
+public:
+    virtual void prepare() = 0;
+    void bake(){};
+    void cut(){};
+    void box(){};
+};
+
+class CheesePizza : public Pizza {
+public:
+    void prepare() override{};
+};
+
+class GreekPizza : public Pizza {
+public:
+    void prepare() override{};
+};
+
+class PepperoniPizza : public Pizza {
+public:
+    void prepare() override{};
+};
+
+unique_ptr<Pizza> orderPizza(PizzaType pizza_type) {
+    unique_ptr<Pizza> pizza;
+
+    // this is a problem - if you wish to add or remove some pizza types - you'll have to change this code
+    switch (pizza_type) {
+    case PizzaType::cheese:
+        pizza = make_unique<CheesePizza>();
+        break;
+    case PizzaType::greek:
+        pizza = make_unique<GreekPizza>();
+        break;
+    case PizzaType::pepperoni:
+        pizza = make_unique<PepperoniPizza>();
+        break;
+    }
+
+    pizza->prepare();
+    pizza->bake();
+    pizza->cut();
+    pizza->box();
+
+    return pizza;
+}
+
+int main() {
+    auto order = orderPizza(PizzaType::cheese);
+
+    return 0;
+}
