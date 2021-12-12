2547a12002d95970fc754a9c0589ae64f721a075
Kudryashov Dmitrii <kudryashovda@simicon.com>
2021-12-12 12:48:13 +0300

    Pattern command - simple undo version

--- Command/command.cpp
+++ Command/command.cpp
@@ -48,12 +48,15 @@ private:
 class Command {
 public:
     virtual void execute() = 0;
+    virtual void undo() = 0;
     virtual string getName() = 0;
 };
 
 class NoCommand : public Command {
 public:
     void execute() override {
+    }
+    void undo() override {
     }
     string getName() override {
         return "NoCommand";
@@ -67,6 +70,9 @@ public:
     }
     void execute() override {
         light_->on();
+    }
+    void undo() override {
+        light_->off();
     }
     string getName() override {
         return "LightOnCommand";
@@ -83,6 +89,9 @@ public:
     }
     void execute() override {
         light_->off();
+    }
+    void undo() override {
+        light_->on();
     }
     string getName() override {
         return "LightOffCommand";
@@ -102,6 +111,9 @@ public:
         stereo_->setCD();
         stereo_->setVolume(11);
     }
+    void undo() override {
+        stereo_->off();
+    }
     string getName() override {
         return "StereoOnWithCDCommand";
     }
@@ -118,6 +130,9 @@ public:
     void execute() override {
         stereo_->off();
     }
+    void undo() override {
+        stereo_->on();
+    }
     string getName() override {
         return "StereoOffCommand";
     }
@@ -126,9 +141,9 @@ private:
     Stereo* stereo_;
 };
 
-class RemoteControl {
+class RemoteControlWithUndo {
 public:
-    RemoteControl() {
+    RemoteControlWithUndo() {
         constexpr int programs_count = 7;
         for (int i = 0; i < programs_count; ++i) {
             onCommands_.push_back(&no_command_);
@@ -142,25 +157,32 @@ public:
     }
     void onButtonWasPressed(int slot) {
         onCommands_.at(slot)->execute();
+        undoCommand = onCommands_.at(slot);
     }
     void offButtonWasPressed(int slot) {
         offCommands_.at(slot)->execute();
+        undoCommand = offCommands_.at(slot);
+    }
+    void undoButtonWasPushed() {
+        undoCommand->undo();
     }
     void printCommands() {
         cout << "\n------ Remote Control ------\n";
         for (int i = 0; i < onCommands_.size(); ++i) {
             cout << "[slot " << i << "] " << onCommands_[i]->getName() << '\t' << offCommands_[i]->getName() << "\n";
         }
+        cout << "Last command was " << undoCommand->getName() << '\n';
     }
 
 private:
     vector<Command*> onCommands_;
     vector<Command*> offCommands_;
     NoCommand no_command_;
+    Command* undoCommand = &no_command_;
 };
 
 int main() {
-    RemoteControl remote;
+    RemoteControlWithUndo remote;
 
     Light livingRoomLight("Living Room");
     LightOnCommand livingRoomLightOn(&livingRoomLight);
@@ -179,6 +201,9 @@ int main() {
     remote.offButtonWasPressed(0);
     remote.onButtonWasPressed(3);
     remote.offButtonWasPressed(3);
+    remote.undoButtonWasPushed();
+
+    remote.printCommands();
 
     return 0;
 }
